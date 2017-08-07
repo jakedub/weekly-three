3df0b8605a9660537183c4c9b234b9f54eef5065
jakedub <jacob.w.moore@gmail.com>
2017-08-07 16:20:03 -0400

    need to fix JS

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-let number = document.getElementsByClassName("number");
-let operate = document.getElementsByClassName("operate");
-// let entry = document.getElementById("entryText");
-let clear = document.getElementById("clear");
-let sub=document.getElementById("sub");
-let multiply = document.getElementById("mul");
-let divide = document.getElementById("divide");
-let add = document.getElementById("plus");
-let memory = 0; //holds the previous number
-let current = 0; //new number
-let operation = 0; //number of the operation
-let total = 0; //the total from calculation
+let keys = querySelectorAll (".keys");
+let operators = ["+", "-", "/", "x"];
+let decimalAdded = false;
+
+for(let i = 0; i < keys.length; i++) {
+	keys[i].onclick = function() {
+		let input = document.querySelector('entry');
+		let inputVal = entry.innerHTML;
+		let buttonVal = this.innerHTML;
+    if(buttonVal === 'C') {
+			input.innerHTML = '';
+			decimalAdded = false;
+		} else if(buttonVal === '=') {
+      let equation = inputVal;
+      let lastChar = equation[equation.length - 1];
+      decimalAdded = false;
+    }
+  };
 
 
 let input = document.querySelector('input');
-document.querySelectorAll('.number').forEach(function (button, index) {
+document.querySelectorAll('.number').forEach(function (button, item) {
   button.addEventListener('click', function () {
-    input.value += index + 1; //showing numbers in different order
+    input.value = item; //showing numbers in different order
   });
 });
 
@@ -26,45 +32,3 @@ document.querySelectorAll('.operate').forEach(function (button, index) {
     entry.value += index + 1; //goes one through five
   });
 });
-
-// function memory(){
-//   if (number.onclick)
-//   return number.value;
-// }
-// function addDigits (){
-//   if (memory = 0);{
-//     number.value;
-//     ;
-//   }
-// }
-//
-// function addition(x,y) { //needs to include a trigger for equal. Callback?
-//
-// }
-//
-// function subtraction(x,y){
-//
-// }
-//
-// function multiply(x,y){
-//
-// }
-//
-// function divide(x,y){
-//
-// }
-//
-// function dot(){
-//
-// }
-//
-// function clear() {
-//   set value to 0
-// }
-//
-// function calculate(){
-//   total = memory - current //if doing subtraction
-//   total = memory * current //if doing multiply
-//   total = memory + current //if doing addition
-//   total = memory / current //if doing divide
-// }
