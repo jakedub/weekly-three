bc09922d2788514df9d9807c0a5806dcc0012e59
jakedub <jacob.w.moore@gmail.com>
2017-08-08 18:17:07 -0400

    calculations work

--- main.js
+++ main.js
@@ -5,60 +5,39 @@
 let number = document.getElementsByClassName("number");
 let operate = document.getElementsByClassName("operate");
 let clear = document.getElementById("clear");
-let sub=document.getElementById("sub");
-let multiply = document.getElementById("mul");
-let divide = document.getElementById("divide");
-let add = document.getElementById("plus");
-let memory = 0; //holds the previous number
-let current = 0; //new number
-let operation = 0; //number of the operation
-let total = 0; //the total from calculation
+// let sub=document.getElementById("sub");
+// let multiply = document.getElementById("mul");
+// let divide = document.getElementById("divide");
+// let add = document.getElementById("plus");
+let display = document.querySelector("input");
+// let plus = eval(x+y);
+// let sub = eval(x-y);
+// let multiply = eval(x*y);
+// let divide = eval(x/y);
 
-
-// //Functions
+//Functions or For Loops
 
 for (let i = 0; i < number.length; i++) {
   number[i].addEventListener("click", function() {
-      let inputValue = entry.querySelector("input").value;
+      let inputValue = entryText.value;
       let buttonValue = this.textContent;
-      if (buttonValue === "C") {
-        entry.innerHTML = "";
-      } else {
-        entry.innerHTML += buttonValue;
-      }
+        entryText.value += buttonValue;
+        console.log(entryText.value);
   })
 }
 
 for (let i = 0; i < operate.length; i++) {
-  operate[i].addEventListener("click", function() {
-      let inputValue = entry.querySelector("input").value;
-      let buttonValue = this.textContent;
-      if (buttonValue === "=") {
-        entry.innerHTML = "";
-      } else {
-        entry.innerHTML += buttonValue;
-      }
+   operate[i].addEventListener("click", function() {
+     let inputValue = entryText.value;
+     let buttonValue = this.textContent;
+     if (buttonValue !== "=") {
+       entryText.value += buttonValue;
+     }
   })
 }
 
-//Need to check if this could work
-let displayNum = "";
-let storedNum = "";
-let operation = 0;
-let queuedOperation = 0;
-let calculationFinished = false;
-function clearDisplay() { //clearing
-    let display = document.querySelector("input");
-    displayNum = "";
-    storedNum = "";
-    operation = 0;
-    queuedOperation = 0;
-    display.value = displayNum;
-
-}
-
 function insertDecimal(dec) {
-    let display = document.querySelector("input");
+    let display = entryText;
     for (i = 0; i < display.value.length; i++) //check to see if decimal
         if (display.value.charAt(i) == '.') {
             return; //if yes then do nothing
@@ -66,20 +45,62 @@ function insertDecimal(dec) {
         display.value += dec;
 }
 
+function total(){
+  entryText.value=eval(entryText.value);
+}
+
+function clearDisplay(){
+  let display = document.querySelector("input");
+  let buttonValue = this.textContent;
+    entry.innerHTML = " ";
+}
+
+// let displayNum = "";
+// let storedNum = "";
+//
+// function calculate(){
+//   let display = document.querySelector("input");
+//     displayedNum = display.value;
+//   let evalDisplay = eval(displayNum),
+//     evalStored = eval(storedNum);
+//   if (display.value === "x") {
+//       displayNum = evalStored * evalDisplay;
+//       }
+// }
+
+
+// Need to check if this could work
+// let displayNum = "";
+// let storedNum = "";
+// // let operation = 0;
+// let storedOperation = 0;
+// let calculationFinished = false;
+// function clearDisplay() { //clearing
+//     let display = document.querySelector("input");
+//     displayNum = "";
+//     storedNum = "";
+//     operation = 0;
+//     storedOperation = 0;
+//     display.value = displayNum;
+//
+// }
+
+
+
 // function operation(command) {
 //     let display = document.querySelector("input"); //selecting the display
 //             evalDisplay = eval(displayNum), //need to check
 //             evalStored = eval(storedNum); //need to check
-//     if (queuedOperation == 0) { //checking for queued operation
+//     if (storedOperation == 0) { //checking for queued operation
 //         storedNum = display.value;
 //     }
-//     else if (queuedOperation == 1) {
+//     else if (storedOperation == 1) {
 //         storedNum = evalStored + evalDisplay;
 //     }
-//     else if (queuedOperation == 2) {
+//     else if (storedOperation == 2) {
 //         storedNum = evalStored - evalDisplay;
 //     }
-//     else if (queuedOperation == 3) {
+//     else if (storedOperation == 3) {
 //         storedNum = evalStored * evalDisplay;
 //     }
 //     if (command == 'add') {
@@ -91,15 +112,15 @@ function insertDecimal(dec) {
 //     if (command == 'multiply') {
 //         operation = 3;
 //     }
-//     queuedOperation = operation;
+//     storedOperation = operation;
 //     display.value = '';
 // }
 //
 // function calculate() {
 //     let display = document.querySelector("input");
-//             displayNum = display.value;
-//     var evalDisplay = eval(displayNum),
-//             evalStored = eval(storedNum);
+//       displayNum = display.value;
+//     let evalDisplay = eval(displayNum),
+//         evalStored = eval(storedNum);
 //     if (operation == 1) {
 //         displayNum = evalStored + evalDisplay;
 //     }
@@ -112,9 +133,4 @@ function insertDecimal(dec) {
 //     display.value = displayNum;
 //     if (operation != 0)
 //         calculationFinished = true;
-//
-//     operation = 0;
-//     queuedOperation = 0;
-//     displayNum = "";
-//     storedNum = "";
 // }
